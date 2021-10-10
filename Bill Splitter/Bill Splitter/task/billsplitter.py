b6967dcc77285108364bd0fad8be02622eba680f
Mariam Lawal <mariam.d.lawal@gmail.com>
2021-10-09 21:52:31 -0400

    Bill Splitter Project Pt. 2/4

---
+++ Bill Splitter/Bill Splitter/task/billsplitter.py
@@ -0,0 +1,31 @@
+# initialize a new list to capture friend names
+friend_list = []
+
+# verify input of number of friends entered, if invalid, print note to user
+try:
+    # retrieve input of number of people joining the dinner party
+    num_friends = int(input("Enter the number of friends joining (including you):\n"))
+
+    # if at least one friend joins, add their name to the friend list
+    if num_friends > 0:
+        print("Enter the name of every friend (including you), each on a new line:")
+        for i in range(num_friends):
+            friend_name = input()
+            friend_list.append(friend_name)
+
+        # initialize a new dictionary to capture all friends attending the dinner party, set the value = 0
+        friend_dict = dict.fromkeys(friend_list, 0)
+
+        # print results from dictionary
+        print(friend_dict)
+
+        # retrieve input of total bill amount
+        bill = float(input("Enter the total bill value:\n"))
+
+    # if no friends are joining, print note to user
+    else:
+        print("No one is joining for the party")
+
+except ValueError:
+    print("No one is joining for the party")
+
