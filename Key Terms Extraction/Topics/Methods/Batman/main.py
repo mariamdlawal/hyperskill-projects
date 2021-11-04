34c86d72713704d8c0faa5a1c317268769a54369
Mariam Lawal <mariam.d.lawal@gmail.com>
2021-11-03 21:19:29 -0400

    Additional exercises toward Key Terms Extraction project

---
+++ Key Terms Extraction/Topics/Methods/Batman/main.py
@@ -0,0 +1,8 @@
+# create a Batman here
+# Your task, on the other hand, is to create an instance
+# of the class Batman and call the method intimidate_villain.
+# Do NOT print anything.
+
+protector = Batman()
+
+protector.intimidate_villain()
